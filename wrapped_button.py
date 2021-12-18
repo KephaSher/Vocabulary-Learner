ccf9d18a4acde80bb39b5a088058016a77b90f6c
Kepha Sher <87000244+KephaSher@users.noreply.github.com>
2021-12-18 00:00:35 -0600

    Add files via upload

---
+++ wrapped_button.py
@@ -0,0 +1,8 @@
+from kivy.uix.button import Button
+class WrappedButton(Button):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(
+            width=lambda *x:
+            self.setter('text_size')(self, (self.width, None)),
+            texture_size=lambda *x: self.setter('height')(self, self.texture_size[1]))
---
+++ wrapped_label.py
@@ -0,0 +1,9 @@
+from kivy.uix.label import Label
+class WrappedLabel(Label):
+    # Based on Tshirtman's answer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.bind(
+            width=lambda *x:
+            self.setter('text_size')(self, (self.width, None)),
+            texture_size=lambda *x: self.setter('height')(self, self.texture_size[1]))
